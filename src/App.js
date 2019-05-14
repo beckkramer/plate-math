ce8e7528898642675fd445057110b3233f174f05
beckkramer <kramer.beck@gmail.com>
2019-05-14 16:09:31 -0500

    Adds counter component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import './logo.svg';
+
+import CounterInput from './components/CounterInput/CounterInput.jsx'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      bar: 0,
+      plates: {
+        w2_5: 0,
+        w5: 0,
+        w10: 0,
+        w15: 0,
+        w25: 0,
+      },
+      total: 0,
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.plates !== this.state.plates) {
+      const totalPlates = this.tallyPlateValues(this.state.plates)
+      this.setState({total: +this.state.bar +totalPlates})
+    }
+  }
+
+  tallyPlateValues = (plates) => {
+    let totalPlates = 0
+    let plate
+    for(plate in plates) {
+      totalPlates += +plates[plate]
+    }
+    return totalPlates*2
+  }
+
+  updatePlateTotal = (weight, value) => {
+    const formattedWeight = String(weight).replace('.', '_')
+    const plate = `w${formattedWeight}`
+    this.setState({plates: {
+      ...this.state.plates,
+      [plate]: value,
+    }})
+  }
+
+  updateBarTotal = (e) => {
+    this.setState({bar: e.target.value})
+    const totalPlates = this.tallyPlateValues(this.state.plates)
+    this.setState({total: +e.target.value +totalPlates})
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
+        <header className="App__header">
+
         </header>
+        <main className="App__main content">
+
+          Pick bar weight: 
+          
+          <label>
+            <input name="bar" onChange={this.updateBarTotal} type="radio" value="35" />
+            35 lbs
+          </label>
+          <label>
+            <input name="bar" onChange={this.updateBarTotal} type="radio" value="45" />
+            45 lbs
+          </label>
+
+          <p>Add the number of plates put on just one side of the bar:</p>
+
+          <CounterInput updateTotal={this.updatePlateTotal} weight={2.5} />
+          <CounterInput updateTotal={this.updatePlateTotal} weight={5} />
+          <CounterInput updateTotal={this.updatePlateTotal} weight={10} />
+          <CounterInput updateTotal={this.updatePlateTotal} weight={15} />
+          <CounterInput updateTotal={this.updatePlateTotal} weight={25} />
+        </main>
+        <footer className="App__footer">
+          Total Weight: {this.state.total}
+        </footer>
       </div>
     );
   }
---
+++ src/components/CounterInput/CounterInput.jsx
@@ -0,0 +1,58 @@
+import React, {Component} from 'react'
+import './CounterInput.scss';
+
+import Minus from '../../icons/Minus.jsx'
+import Plus from '../../icons/Plus.jsx'
+
+class CounterInput extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: 0,
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.value !== this.state.value) {
+      this.sendWeight()
+    }
+  }
+
+  decreaseValue = () => {
+    if (this.state.value > 0) {
+      const newValue = this.state.value - 1
+      this.setState({value: newValue})
+    }
+  }
+
+  increaseValue = () => {
+    const newValue = this.state.value + 1
+    this.setState({value: newValue})
+  }
+
+  sendWeight() {
+    this.props.updateTotal(this.props.weight, this.state.value*this.props.weight)
+  }
+
+  render() {
+    return (
+      <label className="counter-input">
+        <div>
+          {this.props.weight}lbs.
+        </div>
+        <div>
+          <button onClick={this.decreaseValue}>
+            <Minus />
+          </button>
+          <input readOnly value={this.state.value} />
+          <button onClick={this.increaseValue}>
+            <Plus />
+          </button>
+        </div>
+      </label>
+    )
+  }
+}
+
+export default CounterInput
