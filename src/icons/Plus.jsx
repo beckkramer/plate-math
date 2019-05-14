40e694cc08a2a1cdc7c26638a5f3bcda10a73392
beckkramer <kramer.beck@gmail.com>
2019-05-14 16:08:41 -0500

    Adds plus and minus icons and logo

---
+++ src/icons/Plus.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Plus = () => {
+  return (
+    <svg title="add one" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect x="10" width="16" height="4" transform="rotate(90 10 0)" fill="#022C43"/>
+      <rect y="6" width="16" height="4" fill="#022C43"/>
+    </svg>
+  ) 
+}
+
+export default Plus
